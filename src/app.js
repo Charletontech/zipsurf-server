417b4a095e555abae7ebb9ce5d07644eaa75e99b
Charles Uka <121673088+Charletontech@users.noreply.github.com>
2026-02-11 19:04:08 +0100

    Update CORS allowed origins
    Removed localhost:3000 from allowed CORS origins and added https://zipsurf.netlify.app.

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(helmet());
 const allowedOrigins = [
   "http://127.0.0.1:5500", 
   "http://localhost:5500",
-  "http://localhost:3000",
+  "https://zipsurf.netlify.app",
   "https://zipsurf.veetech.site",
   "https://zipsurf.online"
 ];
